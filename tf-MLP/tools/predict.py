0dbbbf0b777bfe138475f86e9d9679d04c68a1dc
Matías <matias.pizarro.o96@gmail.com>
2019-04-04 11:37:57 -0300

    First Commit

---
+++ tf-MLP/tools/predict.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import mlp.fast_predictor as fp
+
+if __name__ == "__main__":
+    params = {"model_dir": "C:/Users/XOF/PycharmProjects/Training_MLP/tf-MLP/mlp",
+              "data_dir": "C:/Users/XOF/PycharmProjects/Training_MLP/MNIST-5000",
+              "device": "/gpu:0",
+              "number_of_classes": 12
+              }
+
+    #     params = { "device" : "/gpu:0",
+    #               "model_dir" : "/home/vision/smb-datasets/MNIST-small/models",
+    #               "data_dir" : "/home/vision/smb-datasets/MNIST-small",
+    #               "number_of_classes" : 10
+    #         }
+    predictor = fp.FastPredictor(params)
+    while True:
+        filename = input("Image: ")
+        prediction = predictor.predict(filename)
+        print(prediction)
